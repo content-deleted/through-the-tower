fcf603a631c8fd79a2fe1720ec91025624ab3ff2
content-deleted <contentdeleted@gmail.com>
2019-04-13 13:55:37 -0700

    feat: clouds

--- tower.js
+++ tower.js
@@ -43,6 +43,7 @@ var Game = /** @class */ (function () {
             effect.setTexture("pallete", pallete);
             effect.setFloat("colorPrecision", 8);
         };
+        this.startCloud();
         //setup update
         this._scene.onBeforeRenderObservable.add(function () { return _this.update(); });
     };
@@ -64,6 +65,36 @@ var Game = /** @class */ (function () {
         if (this._camera != null) {
             this._camera.alpha += 0.01;
         }
+    };
+    Game.prototype.startCloud = function () {
+        var fogTexture = new BABYLON.Texture("https://raw.githubusercontent.com/aWeirdo/Babylon.js/master/smoke_15.png", this._scene);
+        var particleSystem;
+        particleSystem = new BABYLON.ParticleSystem("particles", 2500, this._scene);
+        particleSystem.manualEmitCount = particleSystem.getCapacity();
+        particleSystem.minEmitBox = new BABYLON.Vector3(-25, 2, -25); // Starting all from
+        particleSystem.maxEmitBox = new BABYLON.Vector3(25, 2, 25); // To...
+        particleSystem.particleTexture = fogTexture.clone();
+        var fountain = BABYLON.Mesh.CreateBox("foutain", .01, this._scene);
+        fountain.position.y = -5;
+        fountain.visibility = 0;
+        particleSystem.emitter = fountain;
+        particleSystem.color1 = new BABYLON.Color4(0.8, 0.8, 0.8, 0.1);
+        particleSystem.color2 = new BABYLON.Color4(.95, .95, .95, 0.15);
+        particleSystem.colorDead = new BABYLON.Color4(0.9, 0.9, 0.9, 0.1);
+        particleSystem.minSize = 3.5;
+        particleSystem.maxSize = 5.0;
+        particleSystem.minLifeTime = 9007199254740991;
+        particleSystem.emitRate = 50000;
+        particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_STANDARD;
+        particleSystem.gravity = new BABYLON.Vector3(0, 0, 0);
+        particleSystem.direction1 = new BABYLON.Vector3(0, 0, 0);
+        particleSystem.direction2 = new BABYLON.Vector3(0, 0, 0);
+        particleSystem.minAngularSpeed = -2;
+        particleSystem.maxAngularSpeed = 2;
+        particleSystem.minEmitPower = .5;
+        particleSystem.maxEmitPower = 1;
+        particleSystem.updateSpeed = 0.005;
+        particleSystem.start();
     };
     return Game;
 }());
--- tower.ts
+++ tower.ts
@@ -67,6 +67,9 @@ class Game {
         effect.setFloat("colorPrecision", 8);
     };
 
+    this.startCloud();
+    
+
     //setup update
     this._scene.onBeforeRenderObservable.add(()=>this.update());
   }
@@ -92,6 +95,42 @@ class Game {
       this._camera.alpha += 0.01;
     }
   }
+
+  startCloud(): void {
+    let fogTexture : BABYLON.Texture = new BABYLON.Texture("https://raw.githubusercontent.com/aWeirdo/Babylon.js/master/smoke_15.png", this._scene);
+    let particleSystem;
+    particleSystem = new BABYLON.ParticleSystem("particles", 2500 , this._scene);
+    particleSystem.manualEmitCount = particleSystem.getCapacity();
+    particleSystem.minEmitBox = new BABYLON.Vector3(-25, 2, -25); // Starting all from
+    particleSystem.maxEmitBox = new BABYLON.Vector3(25, 2, 25); // To...
+    
+
+    particleSystem.particleTexture = fogTexture.clone();
+    var fountain = BABYLON.Mesh.CreateBox("foutain", .01, this._scene);
+    fountain.position.y = -5;
+    fountain.visibility = 0;
+    particleSystem.emitter = fountain;
+    
+    particleSystem.color1 = new BABYLON.Color4(0.8, 0.8, 0.8, 0.1);
+    particleSystem.color2 = new BABYLON.Color4(.95, .95, .95, 0.15);
+    particleSystem.colorDead = new BABYLON.Color4(0.9, 0.9, 0.9, 0.1);
+    particleSystem.minSize = 3.5;
+    particleSystem.maxSize = 5.0;
+    particleSystem.minLifeTime = 9007199254740991;
+    particleSystem.emitRate = 50000;
+    particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_STANDARD;
+    particleSystem.gravity = new BABYLON.Vector3(0, 0, 0);
+    particleSystem.direction1 = new BABYLON.Vector3(0, 0, 0);
+    particleSystem.direction2 = new BABYLON.Vector3(0, 0, 0);
+    particleSystem.minAngularSpeed = -2;
+    particleSystem.maxAngularSpeed = 2;
+    particleSystem.minEmitPower = .5;
+    particleSystem.maxEmitPower = 1;
+    particleSystem.updateSpeed = 0.005;
+
+    particleSystem.start();
+  }
+
 }
 
 window.addEventListener('DOMContentLoaded', () => {
